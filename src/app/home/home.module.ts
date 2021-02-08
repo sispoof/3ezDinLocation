f6ec6ff308ddccd88f9755c675eb9ec9107cb7c7
unknown <mbenhamouda099@gmail.com>
2021-02-08 15:45:19 +0100

    la version wé7ed chhnia hia chi9ouloulha? ti 7asilou bath tajribi

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+
+
+@NgModule({
+  declarations: [HomeComponent, NavbarComponent],
+  imports: [
+    CommonModule,
+    HomeRoutingModule
+  ]
+})
+export class HomeModule { }
