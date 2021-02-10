8f544515574e46dc18f63b4182f3cfae4754c097
unknown <mbenhamouda099@gmail.com>
2021-02-10 02:18:55 +0100

    footer

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,11 +4,12 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
-  declarations: [HomeComponent, NavbarComponent],
+  declarations: [HomeComponent, NavbarComponent, FooterComponent],
   imports: [
     CommonModule,
     HomeRoutingModule
